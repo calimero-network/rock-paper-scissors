db56606d61d3492e39b87a939f088ffbc320bc38
Saeed Dadkhah <saeed@zilliqa.com>
2024-05-30 12:43:54 +0330

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/rock-paper-scissors/' : '/'
 })
